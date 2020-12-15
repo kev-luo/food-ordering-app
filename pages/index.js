de5520fb2587acbf80265fcb80fa6b50241113d5
Kevin Luo <kvn.luo@gmail.com>
2020-12-15 13:41:04 -0500

    starter files

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Main = styled.div`
+  color: blue;
+`
+
+export default function Home() {
+  return (
+    <Main>
+      Grubs
+    </Main>
+  )
+}
